9255b9aaf131b1ffe30f9f6fdd2feb32fd3316c8
EmiEpstein <emi_epstein11@hotmail.com>
2026-02-14 17:09:57 -0300

    todas las carpetas armadas ahrexx

--- hola.py
+++
@@ -1,9 +0,0 @@
-#bienvenidos al remix 
-print("Wassup ni")
-
-#primera edicion del codigo papa by piero 
-num=int(input("ingrese un numero entero papaa"))
-print("su numero es ", num) 
-
-print("god daayum")
-soy mujer xd
